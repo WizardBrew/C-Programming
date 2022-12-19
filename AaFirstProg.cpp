e6cc6e4aeb356cf9cf27fc2917f3d71c34196160
WizardBrew <91134716+WizardBrew@users.noreply.github.com>
2022-12-20 00:59:49 +0530

    First.1 Basic Program

---
+++ AaFirstProg.cpp
@@ -0,0 +1,9 @@
+#include<iostream>				//Header 
+
+using namespace std;			//When Not used std Lib.
+								
+int main(){
+	std::cout<<"Hello World \n";		// Without Librery Error.	
+	cout<<"Hello World "<<endl;			// After Including Lib. { cout & endl }
+}
+
