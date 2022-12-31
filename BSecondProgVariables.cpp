472fa2d426f2dba9c791af6ee9ae5b655b91f18e
WizardBrew <91134716+WizardBrew@users.noreply.github.com>
2023-01-01 00:01:00 +0530

    Update and rename BSecondProgDataType.cpp to BSecondProgVariables.cpp

---
+++ BSecondProgVariables.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+						//Credit  iomainip    https://www.programiz.com/cpp-programming/float-double
+using namespace std;
+
+int main(){
+	int x;					// Creating Variable
+	int a;					 
+	float A;				
+	char ch;
+	
+	x = 400;				// Assigning Value to Variable.
+	
+	a= 512345678;				// Variable a  Can be less than 9 Characters
+	A =123456;				// Variable A Can be Less than 8 Charaters After 8 Auto Decimal
+	ch = 'C';
+	
+	cout<<"a is ->"<<a<<endl;
+	
+	cout<<" A is -> "<<A<<endl;
+	
+	cout<<"C is -> "<<ch<<endl;
+}
