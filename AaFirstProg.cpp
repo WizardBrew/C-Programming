aa8350bd040d9cac77e2b0a6ebab29c748d40bed
WizardBrew <91134716+WizardBrew@users.noreply.github.com>
2023-01-13 10:09:53 +0530

    Update AaFirstProg.cpp

--- AaFirstProg.cpp
+++ AaFirstProg.cpp
@@ -4,6 +4,6 @@ using namespace std;			//When Not used std lib.
 								
 int main(){
 	std::cout<<"Hello World \n";		// Without Librery Error.	
-	cout<<"Hello World "<<endl;		// After Including Lib. { cout & endl }
+	cout<<"Hello World "<<endl;		// After Including Lib. { cout & endl } are seamless without scope.
 }
 
